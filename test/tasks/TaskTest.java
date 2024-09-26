d2fe32232eff6b1533f2f403e90b47b3baf60eee
Elena Shilova <e.shilova@vniias.ru>
2024-09-26 23:32:24 +0300

    корректировка по результатам ревью задания 8 спринта

--- test/tasks/TaskTest.java
+++ test/tasks/TaskTest.java
@@ -17,7 +17,7 @@ class TaskTest {
         Task task2 = new Task("Task 1", "task1details", Duration.ofNanos(125412), LocalDateTime.now());
         task2.setId(1);
 
-        assertEquals(task1, task2);
+        assertEquals(task1.getId(), task2.getId());
     }
 
     @Test
@@ -28,7 +28,7 @@ class TaskTest {
         Task task2 = new Task("Task 2", "task2details", Duration.ofNanos(125412), LocalDateTime.now());
         task2.setId(2);
 
-        assertNotEquals(task1, task2);
+        assertNotEquals(task1.getId(), task2.getId());
     }
 
     @Test
